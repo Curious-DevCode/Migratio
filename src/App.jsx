57e32aa72f35485fcf5bc37e8ab5e17fe728148b
Aditya Kulkarni <adikul908@gmail.com>
2025-12-25 03:23:33 +0530

    Fix imports for Vercel deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
+import Header from "./components/Header.jsx";
 import Home from "./pages/Home";
 import Analyze from "./pages/Analyze";
 import Result from "./pages/Result";
